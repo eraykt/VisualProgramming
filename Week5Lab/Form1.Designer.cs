0206d867e436b7c9130a22c0f20c7e1753aadddc
eray <alieraykt@gmail.com>
2023-11-11 02:03:59 +0300

    week5 lab

---
+++ Week5Lab/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Week5Lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            progressBar1 = new ProgressBar();
+            btnStart = new Button();
+            lblTimeLeft = new Label();
+            btnGame = new Button();
+            timerGame = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 526);
+            progressBar1.Maximum = 60;
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(960, 23);
+            progressBar1.Step = 1;
+            progressBar1.Style = ProgressBarStyle.Continuous;
+            progressBar1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            btnStart.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnStart.Location = new Point(415, 228);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(150, 100);
+            btnStart.TabIndex = 1;
+            btnStart.Text = "Süreyi Başlat";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // lblTimeLeft
+            // 
+            lblTimeLeft.AutoSize = true;
+            lblTimeLeft.Font = new Font("Arial Black", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTimeLeft.Location = new Point(310, 470);
+            lblTimeLeft.Name = "lblTimeLeft";
+            lblTimeLeft.Size = new Size(204, 30);
+            lblTimeLeft.TabIndex = 2;
+            lblTimeLeft.Text = "Kalan saniye: 60";
+            lblTimeLeft.Visible = false;
+            // 
+            // btnGame
+            // 
+            btnGame.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnGame.Location = new Point(845, 428);
+            btnGame.Name = "btnGame";
+            btnGame.Size = new Size(123, 34);
+            btnGame.TabIndex = 3;
+            btnGame.Text = "Bana Tıkla";
+            btnGame.UseVisualStyleBackColor = true;
+            btnGame.Visible = false;
+            btnGame.Click += btnGame_Click;
+            // 
+            // timerGame
+            // 
+            timerGame.Tick += timerGame_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(980, 557);
+            Controls.Add(btnGame);
+            Controls.Add(lblTimeLeft);
+            Controls.Add(btnStart);
+            Controls.Add(progressBar1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private ProgressBar progressBar1;
+        private Button btnStart;
+        private Label lblTimeLeft;
+        private Button btnGame;
+        private System.Windows.Forms.Timer timerGame;
+    }
+}
---
+++ Week5Lab/Form1.cs
@@ -0,0 +1,119 @@
+
+using System.Diagnostics;
+
+namespace Week5Lab
+{
+    public partial class Form1 : Form
+    {
+        private int time = 60;
+        private float timeLeft;
+        private bool win = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // center the button
+            btnStart.Left = (this.ClientSize.Width - btnStart.Width) / 2;
+            btnStart.Top = (this.ClientSize.Height - btnStart.Height) / 2;
+
+            lblTimeLeft.Left = (this.ClientSize.Width - lblTimeLeft.Width) / 2;
+
+            progressBar1.Maximum = time * 100;
+            timeLeft = time;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            timer1.Start();
+
+            lblTimeLeft.Visible = true;
+            btnStart.Visible = false;
+
+            btnGame.Visible = true;
+            RandomButtonPosition();
+
+            timerGame.Interval = 500;
+            timerGame.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeLeft -= 1f;
+
+            var value = time * 100 - (int)(timeLeft * 100);
+            progressBar1.Value = value > progressBar1.Maximum ? progressBar1.Maximum : value;
+
+            lblTimeLeft.Text = $"Kalan saniye: {timeLeft} ";
+
+            if (timeLeft <= 10f)
+                lblTimeLeft.ForeColor = Color.Red;
+
+            if (timeLeft <= 0f)
+            {
+                win = false;
+                btnGame_Click(sender, e);
+            }
+        }
+
+        private void timerGame_Tick(object sender, EventArgs e)
+        {
+            RandomButtonPosition();
+        }
+
+        private void RandomButtonPosition()
+        {
+            var random = new Random();
+            var left = random.Next(12, 845);
+            var top = random.Next(12, 428);
+
+            btnGame.Left = left;
+            btnGame.Top = top;
+        }
+
+        private void Restart()
+        {
+            btnStart.Visible = true;
+            progressBar1.Value = progressBar1.Minimum;
+            lblTimeLeft.Text = $"Kalan saniye: 60 ";
+        }
+
+        private void btnGame_Click(object sender, EventArgs e)
+        {
+            btnGame.Visible = false;
+            lblTimeLeft.Visible = false;
+
+            progressBar1.Value = progressBar1.Maximum;
+
+            timer1.Stop();
+            timerGame.Stop();
+            DialogResult res;
+
+            if (win)
+            {
+                res = MessageBox.Show("Tebrikler, " + timeLeft + " saniye kala oyunu bitirdin!",
+                    "Tebrikler!!!",
+                    MessageBoxButtons.RetryCancel);
+            }
+
+            else
+            {
+                res = MessageBox.Show("Süre doldu, kaybettin!",
+                    "Oyun bitti",
+                    MessageBoxButtons.RetryCancel);
+            }
+
+            if (res == DialogResult.Retry)
+                Restart();
+
+            else if (res == DialogResult.Cancel)
+                Close();
+
+            timeLeft = time;
+        }
+    }
+}
