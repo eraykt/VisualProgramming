b87c3224881a26b16e04441f4215a8cc3c25cec4
eray <alieraykt@gmail.com>
2023-11-11 01:00:00 +0300

    week4 lab

---
+++ Week4Lab/Form1.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+
+namespace Week4Lab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            var input = textBox1.Text;
+
+            var param = input.Split('x');
+
+            if (param.Length < 2 || param.Length > 2)
+            {
+                MessageBox.Show("Invalid input");
+                textBox1.Text = "";
+                return;
+            }
+
+            if (!int.TryParse(param[0], out var w) || !int.TryParse(param[1], out var h))
+            {
+                MessageBox.Show("Invalid input");
+                textBox1.Text = "";
+                return;
+            }
+
+            if (w < 300 || h < 300 || w > 1500 || h > 1500)
+            {
+                MessageBox.Show("Out of range");
+                textBox1.Text = "";
+                return;
+            }
+
+            this.Width = w;
+            this.Height = h;
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) btnOk_Click(sender, e);
+
+        }
+    }
+}
